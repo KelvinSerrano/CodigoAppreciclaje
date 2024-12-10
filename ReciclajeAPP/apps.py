b48f1a2a7f6d2788e991a3637346a342c79fcd1b
KelvinSerrano <kelvinserrano1261@gmail.com>
2024-12-10 06:17:36 -0300

    Update apps.py

--- ReciclajeAPP/apps.py
+++ ReciclajeAPP/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ReciclajeappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ReciclajeApp'
+    name = 'ReciclajeAPP'
