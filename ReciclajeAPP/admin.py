0e182439b60f8bc987aaea6b69c2add98c70485c
KelvinSerrano <kelvinserrano1261@gmail.com>
2024-12-11 03:43:44 -0300

    Delete ReciclajeAPP directory

--- ReciclajeAPP/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from ReciclajeApp.models import ClasePunto
-from ReciclajeApp.models import ClaseIncentivo
-from ReciclajeApp.models import ClaseMuni
-# Register your models here.
-admin.site.register(ClasePunto)
-admin.site.register(ClaseIncentivo)
-admin.site.register(ClaseMuni)
